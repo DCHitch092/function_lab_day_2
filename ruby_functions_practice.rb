7b084b575e6121d9588989c4e212ddb814dc09eb
L3n1ad <danieltoth0910@gmail.com>
2019-10-15 15:47:37 +0100

    fixes join string function

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -22,3 +22,9 @@ def length_of_string(test_string)
   test_string = "A string of length 21"
   return test_string.length
 end
+
+def join_string( string_1, string_2)
+  string_1 = "Mary had a little lamb, "
+  string_2 = "its fleece was white as snow"
+  return string_1 + string_2
+end
